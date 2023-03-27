ad86a0d7f7f8e9da5f84a8875ad5bfbff5270239
kauittt <tangkhaiminh2002@gmail.com>
2023-03-27 17:15:31 +0700

    html/css landing page, survey: done

---
+++ .history/assets/js/survey_20230327164148.js
@@ -0,0 +1,49 @@
+const survey = {
+    items: {
+        item: {
+            heading: "Bạn biết tới Duolingo từ đâu?";
+            choices: {
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/947546a876aaea3a9811abf4cca1b618.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+                
+            }
+        }
+    }
+}
---
+++ .history/assets/js/survey_20230327164740.js
@@ -0,0 +1,60 @@
+const survey = {
+    items: {
+        item: {
+            heading: "Bạn biết tới Duolingo từ đâu?";
+            choices: {
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg",
+                    desc: "TV"
+                },
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/947546a876aaea3a9811abf4cca1b618.svg",
+                    desc: "Bạn bè/Gia đình"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+                choice: {
+                    url: "https://d35aaqx5ub95lt.cloudfront.net/images/hdyhau/b2a0faf7b835cf2ab9a75afe033fdad9.svg"
+                    desc: "TV"
+                }
+
+            }
+        }
+    }
+}
+
+async function test() {
+    console.log("work2");
+    const res = await fetch(
+        "https://apizingmp3.vercel.app/api/video?id=Z66IU9I6"
+    );
+    const { data } = await res.json();
+    console.log(data);
+}
+console.log("work1");
+test();
